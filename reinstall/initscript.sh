305bb4a48aa8449a4d4c8cb4efb593be08ed98ce
Kishan Reddy <kishanreddy.kethu@gmail.com>
2014-02-01 18:47:26 +0530

    Add jupiter and grub settings

--- reinstall/initscript.sh
+++ reinstall/initscript.sh
@@ -25,6 +25,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub |sudo apt-key
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo add-apt-repository -y ppa:nilarimogard/webupd8
 sudo add-apt-repository ppa:yannubuntu/boot-repair -y
+sudo add-apt-repository ppa:jolicloud-team/ppa
 sudo apt-get update
 sudo apt-get install nautilus-open-terminal
 nautilus -q
@@ -33,4 +34,8 @@ sudo apt-get install google-chrome-stable -y
 sudo apt-get install ap-hotspot -y
 sudo apt-get upgrade
 sudo apt-get install boot-repair -y
+sudo apt-get install jupiter
 su -c "cat fstabentry.txt >> /etc/fstab;subl /etc/fstab"
+echo "Opening the grub file please change linux default to quiet splash acpi_backlight=vendor"
+su -c "subl /etc/default/grub"
+sudo update-grub
