a0ee5703bf5c500c4f04e98405fdfe4b8ffc7c1f
Kishan Reddy <kishanreddy.kethu@gmail.com>
2014-07-22 00:15:46 +0530

    Add MySQLdb

--- reinstall/13.04/initscript.sh
+++ reinstall/13.04/initscript.sh
@@ -55,6 +55,7 @@ sudo apt-get install vlan
 sudo apt-get install gstreamer0.10 -y
 sudo apt-get install python-pip
 sudo apt-get install python-scapy
+sudo apt-get install python-mysqldb
 sudo gem install rhc
 rhc setup
 wget http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.336/install_flash_player_11_linux.x86_64.tar.gz
